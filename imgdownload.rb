814d5b8e23b79dc9cd13e80ea7eaf87fc7f0ffee
Ryosuke KUTSUNA <ryosuke@deer-n-horse.jp>
2017-12-19 17:36:14 +0900

    一部画像のダウンロードができてない問題にとりあえず対処するダウンローダーを作成。

---
+++ imgdownload.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+#
+# imgdownload.rb
+#
+
+require 'open-uri'
+require 'nokogiri'
+require 'uconv'
+
+def parseBFile(file)
+  imagesdata = Array.new
+  doc = Nokogiri::HTML(Uconv.euctou8(open(file).read),nil,'UTF-8')
+  @article = doc.xpath('//div[@class="blog-text-text"]')
+  @article.xpath('//img[@class="post_img_design2"]').each do |i|
+    imagesdata << i.attribute("src").value
+  end
+  @article.xpath('//img[@class="post_img_design"]').each do |i|
+    imagesdata << i.attribute("src").value
+  end
+  @article.xpath('//img[@class="post_image"]').each do |i|
+    imagesdata << i.attribute("src").value
+  end
+
+  return imagesdata
+end
+
+def downloadImages(images, savedir)
+  images.each do |f|
+    savefile = File.basename(f)
+    open("#{savedir}/images/#{savefile}", "wb") do |o|
+      open("#{f}") do |d|
+        o.write(d.read)
+      end
+    end
+    puts "Save: #{savedir}/images/#{savefile}"
+  end
+end
+
+savedir = ARGV[0]
+bfile = ARGV[1]
+if bfile == nil
+  puts "bfile not exist."
+  exit 1
+end
+
+if savedir == nil
+  puts "savedir not exist."
+  exit 1
+end
+
+unless Dir.exist?(savedir)
+    Dir.mkdir(savedir)
+end
+unless Dir.exist?("#{savedir}/images")
+    Dir.mkdir("#{savedir}/images")
+end
+
+images = parseBFile(bfile)
+downloadImages(images, savedir)
